371a5356ffb1f23b9f48b20e6043292d5e55c655
caduhsn <148654410+caduhsn@users.noreply.github.com>
2025-09-01 19:10:10 -0300

    Update desafio_heroi.js

--- desafio_heroi.js
+++ desafio_heroi.js
@@ -23,7 +23,7 @@
 // "O Herói de nome **{nome}** está no nível de **{nivel}**"
 
  let namePlayer = "Newbie"
- let experiencePlayer = 0001
+ let experiencePlayer = 1000
 
 if (experiencePlayer <= 1000) {
     console.log("O Heroi " + namePlayer + " está no nivel: Ferro")
