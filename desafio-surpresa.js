fd3e9d0ed6dec45f9fe2c4e425be6cef93438075
caduhsn <148654410+caduhsn@users.noreply.github.com>
2025-09-01 18:11:13 -0300

    Create desafio-surpresa.js

---
+++ desafio-surpresa.js
@@ -0,0 +1,24 @@
+// lista de Pokemons
+let pokemons = ["POOCHYENA", "ZIGZAGOON", "DRAGONITE", "DRAGONITE", "DRAGONITE", "POOCHYENA", "WURMPLE"]
+let statusPokemon = [ 
+// POKEMON    Nivel  Gen  HPMin/  HPMax
+["POOCHYENA", "Lv2", "M", "13/ ", "13"],
+["ZIGZAGOON", "Lv2", "F", "13/ ", "13"],
+["DRAGONITE", "Lv5", "M", "25/ ", "25"],
+["DRAGONITE", "Lv5", "F", "24/ ", "24"],
+["DRAGONITE", "Lv5", "F", "24/ ", "24"],
+["POOCHYENA", "Lv3", "F", "15/ ", "15"],
+["WURMPLE",   "Lv2", "M", "07/ ", "14"]
+]
+// Cadastro
+let cadastro = "Cadastrado com sucesso!"
+
+// Chamando no Console 
+// POOCHYENA - Lv2 (M) 13/ 13 - Cadastrado com sucesso!
+console.log(statusPokemon[0][0] + " - " + statusPokemon[0][1] + " (" + statusPokemon[0][2] + ") " + statusPokemon[0][3] + statusPokemon[0][4] + " - " + cadastro)
+console.log(statusPokemon[1][0] + " - " + statusPokemon[1][1] + " (" + statusPokemon[1][2] + ") " + statusPokemon[1][3] + statusPokemon[1][4] + " - " + cadastro)
+console.log(statusPokemon[2][0] + " - " + statusPokemon[2][1] + " (" + statusPokemon[2][2] + ") " + statusPokemon[2][3] + statusPokemon[2][4] + " - " + cadastro)
+console.log(statusPokemon[3][0] + " - " + statusPokemon[3][1] + " (" + statusPokemon[3][2] + ") " + statusPokemon[3][3] + statusPokemon[3][4] + " - " + cadastro)
+console.log(statusPokemon[4][0] + " - " + statusPokemon[4][1] + " (" + statusPokemon[4][2] + ") " + statusPokemon[4][3] + statusPokemon[4][4] + " - " + cadastro)
+console.log(statusPokemon[5][0] + " - " + statusPokemon[5][1] + " (" + statusPokemon[5][2] + ") " + statusPokemon[5][3] + statusPokemon[5][4] + " - " + cadastro)
+console.log(statusPokemon[6][0] + " - " + statusPokemon[6][1] + " (" + statusPokemon[6][2] + ") " + statusPokemon[6][3] + statusPokemon[6][4] + " - " + cadastro)
